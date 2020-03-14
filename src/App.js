2f0272c53b823dc3c765ce8e85e7eeafa271a057
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-03-13 22:16:42 -0400

    initialized starter branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import BookContextProvider from './contexts/BookContext';
-import Navbar from './components/Navbar';
-import BookList from './components/BookList';
-import NewBookForm from './components/BookForm';
 
 function App() {
-  return (
-    <div className="App">
-      <BookContextProvider>
-        <Navbar />
-        <BookList />
-        <NewBookForm />
-      </BookContextProvider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
